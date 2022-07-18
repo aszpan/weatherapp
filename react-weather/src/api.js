bb448e660594f87b9c7f1b27ff36899c0d5a3011
Ashley Szpanelewski <ashleyszp@gmail.com>
2022-07-17 22:51:56 -0400

    Added first API to autocomplete city names. Need to finish methods

---
+++ react-weather/src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3465838548mshe6d0b6031276d02p1e2c73jsnfe0db73becab',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
--- react-weather/src/components/search.js
+++ react-weather/src/components/search.js
@@ -1,5 +1,39 @@
-const Search = () => {
-    return ('Hello')
+import { AsyncPaginate } from "react-select-async-paginate";
+import React, { useEffect, useState } from "react";
+import  { GEO_API_URL, geoApiOptions } from '../api';
+
+
+
+const Search = ({onSearchChange}) => {
+    
+    const [search, setSearch] = useState(null);
+    
+    //will use input to pass into fetch method into url to get the data
+    const loadOptions = (inputValue) => {
+        //specified a minimum population for better search results
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
+    
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+    
+    return (
+       <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+
+            //load options will fill autocomplete in "search for city"
+            loadOptions={loadOptions}
+
+       /> 
+    )
 }
 
 export default Search;
