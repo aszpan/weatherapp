a9a8a4c53b03249b106e255a13c62797bc2add7a
Ashley Szpanelewski <ashleyszp@gmail.com>
2022-07-23 22:17:14 -0400

    Updated current-weather css file

--- react-weather/src/components/current-weather/current-weather.js
+++ react-weather/src/components/current-weather/current-weather.js
@@ -4,10 +4,14 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Detroit</p>
-                <p className="weather-description">Sunny</p>
+                <div>
+                    <p className="city">Detroit</p>
+                    <p className="weather-description">Sunny</p>
+
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png"></img>
             </div>
-            <img alt="weather" className="weather-icon" src="icons/01d.png"></img>
+            
         </div>
     )
 }
